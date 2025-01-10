843db23bedb4f4799c6cbb543821cdbf04f68534
Сергей Берсенев <bersenev@mail.ru>
2025-01-10 12:09:21 +0300

    First success

--- users/main.py
+++ users/main.py
@@ -1,9 +1,36 @@
 import requests
 
 
-payload = {"username": "corpApiUser", "password": "1QAZxsw2"}
-headers = {"Content-Type": "application/json"}
-r = requests.post("https://testlink.alfabank.ru/platform/create-user-corp/login", headers = headers, data = payload)
+# headers = {"Content-Type": "application/json",
+#            "Postman-Token": "fe824de0-c78b-430f-8ad5-f5104f41b919",
+#            "User-Agent": "PostmanRuntime/7.43.0",
+#            "Accept": "*/*",
+#            "Host": "testlink.alfabank.ru",
+#            "Accept-Encoding": "gzip, deflate, br",
+#            "Connection": "keep-alive"
+#            }
+# cookies = {
+#     "JSESSIONID": "11848A5B3BBF23CD19C6290CEA4EF9FD",
+#     "SERVER_ID": "!9slRqH0+D3DA7jXx0gHPNyZdOveuXhZU5Vf8LonHo5CQ0c0POZBKSdUyUrAV9Uy4pmeABV6/20Njgw=="
+# }
+# data = {
+#     "username": "corpApiUser",
+#     "password": "1QAZxsw2"
+# }
+
+# response1 = requests.post("https://testlink.alfabank.ru/platform/create-user-corp/login", cookies = cookies, headers = headers, data = data)
+# print(response1.status_code)
+# print(response1.text)
+# print(response1.headers.get("tokenforcreatingcorpuser"))
+
+cookies = {
+    "SERVER_ID": "!9slRqH0+D3DA7jXx0gHPNyZdOveuXhZU5Vf8LonHo5CQ0c0POZBKSdUyUrAV9Uy4pmeABV6/20Njgw=="
+}
+
+headers = {
+    "TokenForCreatingCorpUser": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjb3JwQXBpVXNlciIsImV4cCI6MTc2ODAzNTIwM30.rxTbY0tnf-sig5LUmtooZ8p0rHZkcBjHCDasGuHWykjMqsmvyR56PJvYtW-JN078O4t7a-E1PGO7BYYUcHtwmw"
+    }
+r = requests.get("https://testlink.alfabank.ru/platform/create-user-corp/create-basic-test-data", headers=headers, cookies=cookies)
 
 print(r.status_code)
 print(r.text)
